9e38b2f315e74a6c7457cd2071b63a63b9ff41d7
Juanigith <ji.altamirano@alumno.um.edu.ar>
2026-05-22 00:54:03 +0000

    programitas

---
+++ Clases/ejerciciooperaciones.py
@@ -0,0 +1,10 @@
+print(7 + 3 * 2)
+print((7+3) * 2)
+print(20%4)
+print(20//4)
+print(20/4)
+print(20%3)
+
+
+edad = 10
+print(eda)
---
+++ Clases/triangulo.py
@@ -0,0 +1,11 @@
+lado_a = float(input("Lado A del triángulo: "))
+lado_b = float(input("Lado B del triángulo: "))
+lado_c = float(input("Lado C del triángulo: "))
+
+equilatero = lado_a == lado_b and lado_b == lado_c
+
+isoceles = lado_a == lado_b or lado_b == lado_c or lado_c == lado_a
+
+triangulo_valido = (lado_a < lado_b + lado_c) and (lado_b < lado_a + lado_b) and (lado_c < lado_a + lado_b)
+
+print(f"triangulo válido: {triangulo_valido}, equilatero: {equilatero}, isóceles: {isoceles}")
